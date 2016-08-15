93ae673396f0a708325d1d37a2ca5cc38bcf2b6a
Duane Leem <duaneleem@alumni.ggu.edu>
2016-08-15 15:33:48 -0700

    Added new designation ISRP and encapsulated most sections to their own HTML file using ngInclude.

---
+++ assets/js/angular/directives.js
@@ -0,0 +1,10 @@
+(function() {
+    angular.module("iahsp2017")
+        .directive("sectionKeynote", [function() {
+            return {
+                restrict: 'A',
+                templateUrl: "assets/js/angular/directives/keynote.html"
+            }; // return
+        }]) // siteHeader
+    ; //angular.module("iahsp2017")
+})(); // function()
