a200d7ee2fdcbbb8fe8bf6bc1e1b2d6e7c5c2121
Duane Leem <duaneleem@alumni.ggu.edu>
2016-11-14 14:14:24 -0800

    Added USC Certified.

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -4,7 +4,9 @@
 /* Mobile L + */
 enquire.register("screen and (max-width: 425px)", {
     match : function() {
-        // IAHSP
+        /* ------------------------------------------
+            1st Slide
+        ------------------------------------------ */
         $("#revolution-layer-0")
             .attr("data-hoffset", "0")
             .attr("data-voffset", "0")
@@ -37,7 +39,9 @@ enquire.register("screen and (max-width: 425px)", {
 // 426px +
 enquire.register("screen and (min-width: 426px)", {
     match : function() {
-        // IAHSP
+        /* ------------------------------------------
+            1st Slide
+        ------------------------------------------ */
         $("#revolution-layer-0")
             .attr("data-hoffset", "0")
             .attr("data-voffset", "0")
@@ -57,6 +61,25 @@ enquire.register("screen and (min-width: 426px)", {
         $("#revolution-layer-3")
             .attr("data-hoffset", "20")
             .attr("data-voffset", "-150")
+        ; // $("revolution-layer-3")
+
+        /* ------------------------------------------
+            2nd Slide
+        ------------------------------------------ */
+        // Logo
+        $("#revolution-layer-0-2")
+            .attr("data-hoffset", "0")
+            .attr("data-voffset", "0")
+            .attr("data-x", "center")
+            .attr("data-y", "140")
+        ; // $("revolution-layer-1")
+
+        // Verbiage
+        $("#revolution-layer-1-2")
+            .attr("data-hoffset", "0")
+            .attr("data-voffset", "100")
+            .attr("data-x", "center")
+            .attr("data-y", "400")
         ; // $("revolution-layer-3")
     }
 });
