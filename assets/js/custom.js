8e7a1db01a5636eca4611eb5cca461c08219a552
Duane Leem <duaneleem@alumni.ggu.edu>
2016-11-18 10:48:31 -0800

    Added YouTube video of conference.

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -33,6 +33,16 @@ enquire.register("screen and (max-width: 425px)", {
             .attr("data-hoffset", "20")
             .attr("data-voffset", "190")
         ; // $("revolution-layer-3")
+
+        /* ------------------------------------------
+            2nd Slide
+        ------------------------------------------ */
+        $("#revolution-layer-1-0")
+            .attr("data-hoffset", "0")
+            .attr("data-voffset", "0")
+            .attr("data-x", "center")
+            .attr("data-y", "center")
+        ; // $("revolution-layer-1")
     }
 });
 
@@ -67,6 +77,16 @@ enquire.register("screen and (min-width: 426px)", {
             .attr("data-hoffset", "20")
             .attr("data-voffset", "-90")
         ; // $("revolution-layer-3")
+
+        /* ------------------------------------------
+            2nd Slide
+        ------------------------------------------ */
+        $("#revolution-layer-1-0")
+            .attr("data-hoffset", "0")
+            .attr("data-voffset", "0")
+            .attr("data-x", "center")
+            .attr("data-y", "center")
+        ; // $("revolution-layer-1")
     }
 });
 
@@ -81,3 +101,231 @@ jQuery('#topMain').onePageNav({
     filter: ':not(.external)',
     easing: 'easeInOutExpo'
 });
+
+
+/* ==============================================================
+    Slider Revolution | Hero
+============================================================== */
+jQuery(document).ready(function() {
+	var revapi;
+
+	/**
+		@HALFSCREEN SLIDER
+	**/
+	if(jQuery(".fullwidthbanner").length > 0) {
+
+	   revapi = jQuery('.fullwidthbanner').revolution({
+			delay:9000,
+			startwidth:1170,
+			startheight:500,
+			hideThumbs:10,
+
+			thumbWidth:100,
+			thumbHeight:50,
+			thumbAmount:5,
+
+			navigationType:"both",
+			navigationArrows:"solo",
+			navigationStyle:"round",
+
+			touchenabled:"on",
+			onHoverStop:"on",
+
+			navigationHAlign:"center",
+			navigationVAlign:"bottom",
+			navigationHOffset:0,
+			navigationVOffset:0,
+
+			soloArrowLeftHalign:"left",
+			soloArrowLeftValign:"center",
+			soloArrowLeftHOffset:20,
+			soloArrowLeftVOffset:0,
+
+			soloArrowRightHalign:"right",
+			soloArrowRightValign:"center",
+			soloArrowRightHOffset:20,
+			soloArrowRightVOffset:0,
+
+			shadow:1,
+			fullWidth:"on",
+			fullScreen:"off",
+
+			stopLoop:"off",
+			stopAfterLoops:-1,
+			stopAtSlide:-1,
+
+
+			shuffle:"off",
+
+			autoHeight:"off",
+			forceFullWidth:"off",
+
+			hideThumbsOnMobile:"off",
+			hideBulletsOnMobile:"on",
+			hideArrowsOnMobile:"on",
+			hideThumbsUnderResolution:0,
+
+			hideSliderAtLimit:0,
+			hideCaptionAtLimit:768,
+			hideAllCaptionAtLilmit:0,
+			startWithSlide:0,
+			fullScreenOffsetContainer: ""
+		});
+
+		// Used by styleswitcher onle - delete this on production!
+		jQuery("#is_wide, #is_boxed").bind("click", function() { revapi.revredraw(); });
+	}
+
+
+	/**
+		@FULLSCREEN SLIDER
+	**/
+	if(jQuery(".fullscreenbanner").length > 0) {
+
+		var tpj=jQuery;				
+		tpj.noConflict();				
+		var revapi25;
+		
+		tpj(document).ready(function() {
+						
+			if(tpj('.fullscreenbanner').revolution == undefined) {
+				revslider_showDoubleJqueryError('.fullscreenbanner');
+			} else {
+				revapi25 = tpj('.fullscreenbanner').show().revolution({
+					delay:9000,
+					startwidth:1200,
+					startheight:700,
+					hideThumbs:0,
+					
+					thumbWidth:100,
+					thumbHeight:50,
+					thumbAmount:4,
+					
+					navigationType:"bullet",
+                    navigationArrows:"solo",
+                    navigationStyle:"round",
+					
+					touchenabled:"on",
+					onHoverStop:"on",
+					
+					navigationHAlign:"center",
+					navigationVAlign:"bottom",
+					navigationHOffset:0,
+					navigationVOffset:0,
+
+					soloArrowLeftHalign:"left",
+					soloArrowLeftValign:"center",
+					soloArrowLeftHOffset:20,
+					soloArrowLeftVOffset:0,
+
+					soloArrowRightHalign:"right",
+					soloArrowRightValign:"center",
+					soloArrowRightHOffset:20,
+					soloArrowRightVOffset:0,
+							
+					shadow:1,
+					fullWidth:"off",
+					fullScreen:"on",
+
+					stopLoop:"off",
+					stopAfterLoops:0,
+					stopAtSlide:1,
+
+					
+					shuffle:"off",
+					
+											
+					forceFullWidth:"on",						
+					fullScreenAlignForce:"off",						
+					hideThumbsOnMobile:"off",
+					hideBulletsOnMobile:"on",
+					hideArrowsOnMobile:"on",
+					hideThumbsUnderResolution:0,
+					
+					hideSliderAtLimit:0,
+					hideCaptionAtLimit:768,
+					hideAllCaptionAtLilmit:0,
+					startWithSlide:0,
+					fullScreenOffsetContainer: "header, .pagetitlewrap"	
+				});
+
+			// Used by styleswitcher onle - delete this on production!
+			jQuery("#is_wide, #is_boxed").bind("click", function() { revapi25.revredraw(); });
+
+			}
+		});	//ready
+
+	}
+
+
+	/**
+		@KEN BURNS
+	**/
+	if(jQuery(".fullscreenbanner.ken-burns").length > 0) {
+
+		revapi = jQuery('.fullwidthbanner').revolution({
+			dottedOverlay:"none",
+			delay:9000,
+			startwidth:1170,
+			startheight:400,
+			hideThumbs:200,
+			
+			thumbWidth:100,
+			thumbHeight:50,
+			thumbAmount:5,
+			
+			navigationType:"bullet",
+			navigationArrows:"solo",
+			navigationStyle:"round",
+			
+			touchenabled:"on",
+			onHoverStop:"off",
+			
+			navigationHAlign:"center",
+			navigationVAlign:"bottom",
+			navigationHOffset:0,
+			navigationVOffset:0,
+
+			soloArrowLeftHalign:"left",
+			soloArrowLeftValign:"center",
+			soloArrowLeftHOffset:20,
+			soloArrowLeftVOffset:0,
+
+			soloArrowRightHalign:"right",
+			soloArrowRightValign:"center",
+			soloArrowRightHOffset:20,
+			soloArrowRightVOffset:0,
+					
+			shadow:1,
+			fullWidth:"on",
+			fullScreen:"off",
+
+			stopLoop:"off",
+			stopAfterLoops:-1,
+			stopAtSlide:-1,
+
+			
+			shuffle:"off",
+			
+			autoHeight:"off",						
+			forceFullWidth:"off",						
+									
+			hideThumbsOnMobile:"off",
+			hideBulletsOnMobile:"off",
+			hideArrowsOnMobile:"off",
+			hideThumbsUnderResolution:0,
+			
+			hideSliderAtLimit:0,
+			hideCaptionAtLimit:0,
+			hideAllCaptionAtLilmit:0,
+			startWithSlide:0,
+			videoJsPath:"http://server.local/revslider/wp-content/plugins/revslider/rs-plugin/videojs/",
+			fullScreenOffsetContainer: ""
+		});
+
+		// Used by styleswitcher onle - delete this on production!
+		jQuery("#is_wide, #is_boxed").bind("click", function() { revapi.revredraw(); });
+
+	}
+
+});	//ready
